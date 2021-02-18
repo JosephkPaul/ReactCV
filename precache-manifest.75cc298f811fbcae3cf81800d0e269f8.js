76f5369187fd7b3eda6cf786d5fda3f81cd171eb
JosephkPaul <josephkpaul20@gmail.com>
2021-02-18 10:54:49 +0530

    Updates

---
+++ precache-manifest.75cc298f811fbcae3cf81800d0e269f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7705bb0f6b211413c1c74f28ef67283a",
+    "url": "/ReactCV/index.html"
+  },
+  {
+    "revision": "fdf5b81cf730716fe5e3",
+    "url": "/ReactCV/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "81448f1c69220d2d9712",
+    "url": "/ReactCV/static/js/2.aafb7189.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/ReactCV/static/js/2.aafb7189.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdf5b81cf730716fe5e3",
+    "url": "/ReactCV/static/js/main.9c5cb1b3.chunk.js"
+  },
+  {
+    "revision": "84a0d91358c7d3f24930",
+    "url": "/ReactCV/static/js/runtime-main.3173cc56.js"
+  }
+]);
